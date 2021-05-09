79789c59d606dda193dda02945fcbd0d0e4f439a
vshal-ch <chirumamilla.vishal2019@vitstudent.ac.in>
2021-05-09 16:02:12 +0530

    After adding Flip Animation

--- script.js
+++ script.js
@@ -22,96 +22,67 @@ const generateRandomPlaces = () => {
 };
 
 places_array=generateRandomPlaces();
-
-let cards=document.querySelectorAll('.card');
+let cards=document.querySelectorAll('.backpart');
 let emojis=['url("./emojis/1F603.svg") no-repeat center, linear-gradient(0deg,#1e1e1e,#1e1e1e)','url("./emojis/1F606.svg") no-repeat center, linear-gradient(0deg,#1e1e1e,#1e1e1e)','url("./emojis/1F609.svg") no-repeat center, linear-gradient(0deg,#1e1e1e,#1e1e1e)','url("./emojis/1F642.svg") no-repeat center, linear-gradient(0deg,#1e1e1e,#1e1e1e)','url("./emojis/1F643.svg") no-repeat center, linear-gradient(0deg,#1e1e1e,#1e1e1e)','url("./emojis/1F928.svg") no-repeat center, linear-gradient(0deg,#1e1e1e,#1e1e1e)'];
-
-
 for(let i=0;i<6;i++){
     cards[places_array[i][0]].style.background=emojis[i];
     cards[places_array[i][1]].style.background=emojis[i];
 }
-
-const restartGame = () => {
-    places_array=generateRandomPlaces();
-
-    console.log(places_array);
-
-    cards=document.querySelectorAll('.card');
-    emojis=['url("./emojis/1F603.svg") no-repeat center, linear-gradient(0deg,#1e1e1e,#1e1e1e)','url("./emojis/1F606.svg") no-repeat center, linear-gradient(0deg,#1e1e1e,#1e1e1e)','url("./emojis/1F609.svg") no-repeat center, linear-gradient(0deg,#1e1e1e,#1e1e1e)','url("./emojis/1F642.svg") no-repeat center, linear-gradient(0deg,#1e1e1e,#1e1e1e)','url("./emojis/1F643.svg") no-repeat center, linear-gradient(0deg,#1e1e1e,#1e1e1e)','url("./emojis/1F928.svg") no-repeat center, linear-gradient(0deg,#1e1e1e,#1e1e1e)'];
-
-
-    for(let i=0;i<6;i++){
-        cards[places_array[i][0]].style.background=emojis[i];
-        cards[places_array[i][1]].style.background=emojis[i];
-    }
-    for(let i=0;i<12;i++){
-        frontparts[i].style.backgroundColor='#1e1e1e';
-    }
-
-    console.log(cards[1].style.background);
-}
-
 let frontparts=document.querySelectorAll('.frontpart');
-
 let clicked=null;
 let index=null;
 let flag=false;
 for(let i=0;i<12;i++){
-    frontparts[i].addEventListener('click',() => {
-
+    frontparts[i].addEventListener('click', () => {
         let innerflag=false;
-        if(frontparts[i].style.backgroundColor=='transparent'){
-            innerflag=true;
-        }
-       
-        frontparts[i].style.backgroundColor='transparent';
+        frontparts[i].parentElement.classList.add('active');
         if(flag){
-            let t=index;
-            if(t!=i&&frontparts[i].style.display=='flex'){
-                flag=true;
-                innerflag=true;
-            }
-            if(t==i){
-                frontparts[t].style.backgroundColor='transparent';
-                flag=true;
-            }
-            
-            else if(cards[t].style.backgroundImage!=cards[i].style.backgroundImage){
-                
+            let t=index
+            if(cards[t].style.backgroundImage!=cards[i].style.backgroundImage){
                 setTimeout(function(){
-                    frontparts[t].style.backgroundColor='#1e1e1e';
-                    frontparts[i].style.backgroundColor='#1e1e1e';
+                    frontparts[t].parentElement.classList.remove('active');
+                    frontparts[i].parentElement.classList.remove('active');
                     flag=false;
                     innerflag=true;
-                },500);
-            } 
+                },750);
+            }
             else{
-                frontparts[t].style.pointerEvents='none';
                 frontparts[i].style.pointerEvents='none';
+                frontparts[t].style.pointerEvents='none';
+                cards[i].parentElement.style.pointerEvents='none';
+                cards[t].parentElement.style.pointerEvents='none';
+                setInterval(() => {
+                    str=cards[i].style.backgroundImage;
+                    str1=cards[t].style.backgroundImage
+                    cards[i].style.backgroundImage=str.substr(0,str.length-55)+'linear-gradient(0deg, rgb(1, 250, 30), rgb(1, 250, 30))';
+                    cards[t].style.backgroundImage=str1.substr(0,str.length-55)+'linear-gradient(0deg, rgb(1, 250, 30), rgb(1, 250, 30))';
+                },500);
+                
                 flag=false;
                 innerflag=true;
+                if(checkIfWon()){
+                    alert('You won');
+                    window.onbeforeunload= () => {
+                        return null;
+                    }
+                    location.reload();
+                }
             }
         }
+        frontparts[i].parentElement.classList.add('active');
+        cards[i].classList.add('onscreen');
         index=i;
         if(!(innerflag)){
             flag=true;
-        }
-        if(checkIfWon()){
-            alert('You won');
-            console.log('debug');
-            window.onbeforeunload= () => {
-                return null;
-            }
-            location.reload();
         }
     });
 }
 
 const checkIfWon = () => {
     let c=0;
+
     for(let i = 0;i<12;i++){
-        if(frontparts[i].style.backgroundColor=='transparent'){
+        if(frontparts[i].parentElement.classList.contains('active')){
             c++;
         }
     }
@@ -124,5 +95,5 @@ const checkIfWon = () => {
 }
 
 window.onbeforeunload = () => {
-    return 'Game data will be cleared';
+    return 'Game will be restarted';
 }
